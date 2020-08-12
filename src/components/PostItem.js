bc6a8ee19aa258d9be37f26ee797ba904673152a
demoraes <gabrielmoraes_2013@outlook.com>
2020-08-11 22:21:11 -0400

    component PostItem

--- src/components/PostItem.js
+++ src/components/PostItem.js
@@ -1,17 +1,42 @@
 import React from 'react';
 
-function PostItem() {
+function PostHeader({ author, date }) {
   return (
-    <header>
-      <nav>
-        <img src={logo} alt="facebook" />
-        <div>
-          <span>Meu perfil</span>
-          <i className="material-icons">account_circle</i>
+    <div className="post-header">
+      <img className="avatar" src={author.avatar} />
+      <div className="details">
+        <span>{author.name}</span>
+        <span>{date}</span>
+      </div>
+    </div>
+  );
+}
+
+function PostComments({ comments }) {
+  return (
+    <div className="post-comments">
+      <div className="divider" />
+      {comments.map(comment => (
+        <div key={comment.id} className="comment">
+          <img className="avatar" src={comment.author.avatar} />
+          <p>
+            <span>{comment.author.name}</span>
+            {comment.content}
+          </p>
         </div>
-      </nav>
-    </header>
-  )
+      ))}
+    </div>
+  );
+}
+
+function PostItem({ author, date, content, comments }) {
+  return (
+    <div className="post">
+      <PostHeader author={author} date={date} />
+      <p className="post-content">{content}</p>
+      <PostComments comments={comments} />
+    </div>
+  );
 }
 
 export default PostItem;
